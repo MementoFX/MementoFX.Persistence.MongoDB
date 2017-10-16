a1060596d69daf21148f1f3819672ebf0fcfa11f
Enos Recanati <recanatie@hotmail.it>
2017-10-16 17:53:17 +0200

    MongoDbSingleCollectionEventStore implementation

---
+++ src/MementoFX.Persistence.MongoDB.Tests/MongoDbSingleCollectionEventStoreFixture.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Memento.Messaging;
+using SharpTestsEx;
+using Moq;
+using MongoDB.Driver;
+
+namespace Memento.Persistence.MongoDB.Tests
+{
+    [TestClass]
+    public class MongoDbSingleCollectionEventStoreFixture
+    {
+        [TestMethod]
+        public void Ctor_should_throw_ArgumentNullException_on_null_mongoDatabase_and_value_of_parameter_should_be_mongoDatabase()
+        {
+            var eventDispatcher = new Mock<IEventDispatcher>().Object;
+            Executing.This(() => new MongoDbSingleCollectionEventStore(eventDispatcher, null))
+                .Should()
+                .Throw<ArgumentNullException>()
+                .And
+                .ValueOf
+                .ParamName
+                .Should()
+                .Be
+                .EqualTo("mongoDatabase");
+        }
+
+        [TestMethod]
+        public void Ctor_should_throw_ArgumentNullException_on_null_eventDispatcher_and_value_of_parameter_should_be_eventDispatcher()
+        {
+            var mongoDataBase = new Mock<IMongoDatabase>().Object;
+            Executing.This(() => new MongoDbSingleCollectionEventStore(null, mongoDataBase))
+                .Should()
+                .Throw<ArgumentNullException>()
+                .And
+                .ValueOf
+                .ParamName
+                .Should()
+                .Be
+                .EqualTo("eventDispatcher");
+        }
+
+        [TestMethod]
+        public void Ctor_should_set_MongoDatabase_field()
+        {
+            var bus = new Mock<IEventDispatcher>().Object;
+            var mock = new Mock<IMongoDatabase>().Object;
+            var sut = new MongoDbSingleCollectionEventStore(bus, mock);
+
+            Assert.AreEqual(mock, MongoDbSingleCollectionEventStore.MongoDatabase);
+        }
+    }
+}
---
+++ src/MementoFX.Persistence.MongoDB/MongoDbSingleCollectionEventStore.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Memento.Messaging;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+
+namespace Memento.Persistence.MongoDB
+{
+    public class MongoDbSingleCollectionEventStore : EventStore
+    {
+        public static IMongoClient MongoClient { get; private set; }
+
+        public static IMongoCollection<BsonDocument> MongoCollection { get; private set; }
+
+        public static IMongoDatabase MongoDatabase { get; private set; }
+
+        public MongoDbSingleCollectionEventStore(IEventDispatcher eventDispatcher)
+            : base(eventDispatcher)
+        {
+            if (MongoClient == null)
+            {
+                var connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["EventStore"].ConnectionString;
+                var databaseName = MongoUrl.Create(connectionString).DatabaseName;
+                MongoClient = new MongoClient(connectionString);
+                MongoDatabase = MongoClient.GetDatabase(databaseName);
+                MongoCollection = MongoDatabase.GetCollection<BsonDocument>("DomainEvents");
+            }
+        }
+
+        public MongoDbSingleCollectionEventStore(IEventDispatcher eventDispatcher, IMongoDatabase mongoDatabase)
+            : base(eventDispatcher)
+        {
+            if (mongoDatabase == null)
+                throw new ArgumentNullException("mongoDatabase");
+
+            MongoDatabase = mongoDatabase;
+
+            MongoClient = mongoDatabase.Client;
+            MongoCollection = MongoDatabase.GetCollection<BsonDocument>("DomainEvents");
+        }
+
+        public MongoDbSingleCollectionEventStore(IEventDispatcher eventDispatcher, IMongoDatabase mongoDatabase, string collectionName)
+            : base(eventDispatcher)
+        {
+            if (mongoDatabase == null)
+                throw new ArgumentNullException("mongoDatabase");
+
+            MongoDatabase = mongoDatabase;
+
+            MongoClient = mongoDatabase.Client;
+            MongoCollection = MongoDatabase.GetCollection<BsonDocument>(collectionName ?? "DomainEvents");
+        }
+
+        public override IEnumerable<T> Find<T>(Func<T, bool> filter)
+        {
+            var clrType = typeof(T);
+            var events = new List<T>();
+
+            var builder = Builders<BsonDocument>.Filter;
+
+            var clrTypeFilter = builder.Eq("ClrType", clrType.FullName);
+
+            using (var cursor = MongoCollection.FindSync(clrTypeFilter))
+            {
+                while (cursor.MoveNext())
+                {
+                    var batch = cursor.Current;
+
+                    foreach (var document in batch)
+                    {
+                        var evt = BsonSerializer.Deserialize(document["DomainEvent"].AsBsonDocument, clrType);
+
+                        events.Add((T)evt);
+                    }
+                }
+            }
+
+            return events.Where(filter);
+        }
+
+        public override IEnumerable<DomainEvent> RetrieveEvents(Guid aggregateId, DateTime pointInTime, IEnumerable<EventMapping> eventDescriptors, Guid? timelineId)
+        {
+            var events = new List<DomainEvent>();
+
+            var descriptorsGrouped = eventDescriptors
+                .GroupBy(k => k.EventType);
+
+            foreach (var descriptorsGroup in descriptorsGrouped)
+            {
+                var eventType = descriptorsGroup.Key;
+                var collectionName = eventType.Name;
+
+                var builder = Builders<BsonDocument>.Filter;
+                var filters = new List<FilterDefinition<BsonDocument>>();
+
+
+                var resultFilter = builder.Eq("ClrType", eventType.FullName);
+
+                for (int i = 0; i < descriptorsGroup.Count(); i++)
+                {
+                    var eventDescriptor = descriptorsGroup.ElementAt(i);
+                    var filterBuilt = builder.Eq("DomainEvent." + eventDescriptor.AggregateIdPropertyName, new BsonBinaryData(aggregateId));
+                    filters.Add(filterBuilt);
+                }
+
+                resultFilter = resultFilter & Builders<BsonDocument>.Filter.Or(filters);
+
+                resultFilter = resultFilter & builder.Lte("DomainEvent.TimeStamp", pointInTime);
+
+                if (!timelineId.HasValue)
+                {
+                    resultFilter = resultFilter & builder.Eq("DomainEvent.TimelineId", BsonNull.Value);
+                }
+                else
+                {
+                    resultFilter = resultFilter & (builder.Eq("DomainEvent.TimelineId", BsonNull.Value) | builder.Eq("DomainEvent.TimelineId", new BsonBinaryData(timelineId.Value)));
+                }
+
+                using (var cursor = MongoCollection.FindSync(resultFilter))
+                {
+                    while (cursor.MoveNext())
+                    {
+                        var batch = cursor.Current;
+
+                        foreach (var document in batch)
+                        {
+                            var evt = BsonSerializer.Deserialize(document["DomainEvent"].AsBsonDocument, eventType);
+
+                            events.Add((DomainEvent)evt);
+                        }
+                    }
+                }
+            }
+
+            return events.OrderBy(e => e.TimeStamp);
+        }
+
+        protected override void _Save(DomainEvent @event)
+        {
+            var bsonDocument = new BsonDocument();
+            bsonDocument["ClrType"] = @event.GetType().FullName;
+            bsonDocument["DomainEvent"] = @event.ToBsonDocument();
+
+            MongoCollection.InsertOne(bsonDocument);
+        }
+    }
+}
